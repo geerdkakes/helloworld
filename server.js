3e3d6345498adb2d5af818cdb903048c63eb49cd
Geerd Kakes <geerd@kakes.net>
2019-05-22 17:28:11 +0200

    added console message

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
     response.writeHead(200);
+    console.log("received connection request: " + request.connection.remoteAddress);
       response.end("Hello NEW World!");
 }
 var www = http.createServer(handleRequest);
